67c119743a3ac3b9436e24958ce776d60fdfe874
Cyb3rb34s7 <56477046+cyb3rb34s7@users.noreply.github.com>
2022-01-20 17:06:13 +0530

    Time: 5 ms (39.85%), Space: 5.8 MB (79.80%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -6,6 +6,12 @@ public:
         if(n==1)
             return 1;
         
-        return fib(n-1)+fib(n-2);
+        int f[n+1];
+        f[0]=0;f[1]=1;
+        
+        for(int i = 2; i<=n;i++){
+            f[i]= f[i-1]+f[i-2];
+        }
+        return f[n];
     }
 };
