65f64f730340e10f738697c0e92c4fa519770592
Cyb3rb34s7 <56477046+cyb3rb34s7@users.noreply.github.com>
2022-06-01 09:17:53 +0530

    Time: 5 ms (33.56%), Space: 8.9 MB (99.34%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.cpp
+++ 88-merge-sorted-array/88-merge-sorted-array.cpp
@@ -1,15 +1,25 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        int p1=m-1,p2=n-1,i=m+n-1;
+        int high= m+n-1 , low= m-1,mid=n-1;
         
-        while(p2>=0){
-            if(p1>=0 && nums1[p1]>nums2[p2]){
-                nums1[i--]=nums1[p1--];
-            }
-            else{
-                nums1[i--]=nums2[p2--];
+        while(high>=0){
+            if(low<0 or mid<0)
+                break;
+            nums1[high--] = nums1[low]>nums2[mid] ? nums1[low--] : nums2[mid--] ;
+        }
+        if(low<0)
+        {
+            while(high>=0){
+                nums1[high--]=nums2[mid--];
             }
         }
+        if(mid<0)
+        {
+            while(high>=0){
+                nums1[high--]=nums1[low--];
+            }
+        }
+        
     }
 };
