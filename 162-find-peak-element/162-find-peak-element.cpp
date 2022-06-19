510215ca8f22cf2e476efb5d220d2f3b54893e98
Cyb3rb34s7 <56477046+cyb3rb34s7@users.noreply.github.com>
2022-06-19 15:02:58 +0530

    Time: 4 ms (73.88%), Space: 8.8 MB (89.02%) - LeetHub

--- 162-find-peak-element/162-find-peak-element.cpp
+++ 162-find-peak-element/162-find-peak-element.cpp
@@ -5,9 +5,7 @@ public:
         int low= 0 ; 
         int high = n-1 ; 
         if(n==1) return 0 ; 
-        if(nums[0] > nums[1]) return 0; 
-        if(nums[n-1] > nums[n - 2]) return n - 1; 
-        
+       
         while(low<=high){
             int mid = low + (high-low) / 2 ; 
             
