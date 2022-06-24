c9dfdbd6d9d508b68769b06d5bfc323d3aea3222
Cyb3rb34s7 <56477046+cyb3rb34s7@users.noreply.github.com>
2022-06-24 12:56:02 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (90.14%) - LeetHub

--- 28-implement-strstr/28-implement-strstr.cpp
+++ 28-implement-strstr/28-implement-strstr.cpp
@@ -1,27 +1,22 @@
 class Solution {
 public:
-    int strStr(string s, string k) {
-        int n1=s.size() , n2 = k.size() ; 
-        int i = 0 ; 
-        int res= -1 ; 
-        for(int j = 0 ;j<n1 ; j++ ) {
-            if(s[j]==k[i]){
-                res = j ; 
-                int p = j ; 
-                while(i<n2 and p<n1){
-                    if(s[p]!=k[i])
-                        break; 
-                    p++ ; i++ ; 
-                }
-                if(i==n2)
-                    return res; 
-                else
-                {
-                    i=0 ; 
-                    res=-1 ; 
-                }
+    int strStr(string haystack, string needle) {
+        int i=0 , j = 0 ; 
+        
+        while(i<haystack.size() && j<needle.size()){
+            if(haystack[i]==needle[j]){
+                i++ ; 
+                j++ ;
+                if(j==needle.size())
+                    return i-needle.size() ; 
+            }
+            else
+            {
+                i=i-j+1; 
+                j = 0 ; 
             }
         }
-        return res; 
+        return -1; 
+        
     }
 };
