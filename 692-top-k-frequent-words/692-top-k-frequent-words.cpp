d26c06948563c9f88c6761bd6098499b42df6b5e
Cyb3rb34s7 <56477046+cyb3rb34s7@users.noreply.github.com>
2022-07-08 10:20:01 +0530

    Time: 22 ms (42.43%), Space: 12.8 MB (33.94%) - LeetHub

---
+++ 692-top-k-frequent-words/692-top-k-frequent-words.cpp
@@ -0,0 +1,36 @@
+
+class Solution {
+    struct compare{
+        public:
+            bool operator()(pair<int,string> &p1 , pair<int,string> &p2) {
+        if(p1.first==p2.first)
+                return p1.second>p2.second ;
+            
+
+                return p1.first<p2.first;
+    }
+    };
+public:
+    vector<string> topKFrequent(vector<string>& words, int k) {
+        
+        unordered_map<string,int> mp ; 
+        
+        for(auto x:words)
+            mp[x]++ ; 
+        
+        priority_queue<pair<int,string>, vector<pair<int,string>>,compare>pq;
+        
+        for(auto it:mp){
+            pq.push({it.second,it.first});
+        }
+        vector<string> ans ; 
+        while(k--)
+        {
+            string s = pq.top().second ; 
+            cout<<s<<endl;
+            pq.pop() ; 
+            ans.push_back(s) ;
+        }
+        return ans ; 
+    }
+};
