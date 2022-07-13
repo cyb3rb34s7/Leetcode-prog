ae42953ad3e737d7ac4d2e940a8c3b82fbe4703f
Cyb3rb34s7 <56477046+cyb3rb34s7@users.noreply.github.com>
2022-07-13 23:59:13 +0530

    Time: 4 ms (22.52%), Space: 6.2 MB (30.91%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -1,18 +1,17 @@
 class Solution {
 public:
-    int dp[46] ; 
-    int func(int n){
-        if(n<=2)
-            return n ;
-        if(dp[n]!=-1) return dp[n] ;
-        
-        dp[n-1] =func(n-1) ;
-        dp[n-2] = func(n-2) ; 
-        return dp[n-1]+dp[n-2] ; 
-    }
     int climbStairs(int n) {
-        memset(dp,-1,sizeof(dp));
         
-        return func(n) ; 
+        if(n<=2) return n ; 
+        vector<int>dp(n+1,-1) ; 
+        
+        dp[0] = 0 ;
+        dp[1] = 1; 
+        dp[2] =2 ;
+        
+        for(int i=3;i<=n;i++)
+            dp[i] = dp[i-1]+dp[i-2] ; 
+        
+        return dp[n]; 
     }
 };
