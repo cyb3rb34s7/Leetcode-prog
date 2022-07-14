08750d0ef3864994e90b2228e1100786e34c060e
Cyb3rb34s7 <56477046+cyb3rb34s7@users.noreply.github.com>
2022-07-14 22:30:36 +0530

    Time: 4 ms (30.48%), Space: 6.4 MB (79.29%) - LeetHub

--- 119-pascals-triangle-ii/119-pascals-triangle-ii.cpp
+++ 119-pascals-triangle-ii/119-pascals-triangle-ii.cpp
@@ -3,16 +3,15 @@ public:
     vector<int> getRow(int n) {
         if(n==0)
             return {1} ;
-        if(n==1)
-            return {1,1} ; 
+        
         
         int dp[n+1][n+1] ; 
         memset(dp,0,sizeof(dp)) ;
-        dp[1][1]=1;
+        
         for(int i=0;i<n+1;i++)
             dp[i][0]=1 ; 
         
-        for(int i=2;i<n+1;i++){
+        for(int i=1;i<n+1;i++){
             dp[i][i]=1 ; 
             for(int j=1;j<i;j++){
                 dp[i][j] = dp[i-1][j-1] + dp[i-1][j] ; 
