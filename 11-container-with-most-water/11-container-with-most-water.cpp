3b015a9bb90de47c6986318b7634daca3c75d09d
Cyb3rb34s7 <56477046+cyb3rb34s7@users.noreply.github.com>
2022-07-26 12:38:37 +0530

    Time: 109 ms (80.74%), Space: 59.1 MB (37.66%) - LeetHub

--- 11-container-with-most-water/11-container-with-most-water.cpp
+++ 11-container-with-most-water/11-container-with-most-water.cpp
@@ -1,47 +1,17 @@
-class Solution
-{
-    public:
-        int max(int &a, int &b)
-        {
-            if (a > b)
-                return a;
-            else
-                return b;
+class Solution {
+public:
+    int maxArea(vector<int>& h) {
+        int n = h.size() ; 
+        
+        int i = 0 , j = n-1 ;
+        int res = 0 ; 
+        while(i<=j){
+            int temp = min(h[i],h[j]) * (j-i) ; 
+            res = max(res,temp) ;
+            
+            if(h[i]<h[j]) i++ ;
+            else j-- ;
         }
-    int maxArea(vector<int> &height)
-    {
-        int n = height.size();
-
-        vector<int> maxL(n), maxR(n);
-        int Lmax = -1, Rmax = -1;
-        for (int i = 0; i < n; i++)
-        {
-            Lmax = max(Lmax, height[i]);
-            maxL[i] = Lmax;
-        }
-        for (int i = n - 1; i >= 0; i--)
-        {
-            Rmax = max(Rmax, height[i]);
-
-            maxR[i] = Rmax;
-        }
-        int i = 0, j = n - 1;
-        int maxWater = 0;
-        while (i <= j)
-        {
-            if (maxL[i] < maxR[j])
-            {
-                int temp = maxL[i] *(j - i);;
-                maxWater = max(maxWater, temp);
-                i++;
-            }
-            else
-            {
-                int temp2 = maxR[j] *(j - i);
-                maxWater = max(maxWater, temp2);
-                j--;
-            }
-        }
-        return maxWater;
+        return res ; 
     }
 };
