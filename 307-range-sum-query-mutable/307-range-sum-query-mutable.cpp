d493c22b1e363cb6e09f67d50d83285f7d4fc270
Cyb3rb34s7 <56477046+cyb3rb34s7@users.noreply.github.com>
2022-07-31 12:59:02 +0530

    Time: 637 ms (62.17%), Space: 149.9 MB (94.89%) - LeetHub

--- 307-range-sum-query-mutable/307-range-sum-query-mutable.cpp
+++ 307-range-sum-query-mutable/307-range-sum-query-mutable.cpp
@@ -1,75 +1,30 @@
 class NumArray {
 public:
-    vector<int> tree ;
-    int n ;
+    vector<int>v; //vector to store input vector.
+    int sum; //sum of all element of vector 
     NumArray(vector<int>& nums) {
-        if(nums.size())
-        {
-            n = nums.size() ; 
-            vector<int> t(2*n) ;
-            tree = t ;
+        v=nums;
+        sum=0;
+        for(int i=0;i<nums.size();i++){
+            sum+=nums[i];
         }
-        
-        for(int i=n,j=0 ;i<2*n;i++,j++){
-            tree[i] = nums[j] ;
-        }
-        for(int i =n-1 ;i>0 ;i--){
-            tree[i] = tree[i*2] + tree[(2*i) + 1] ;
-            
-        }
-        
-        
     }
-     
-        
-    
     
     void update(int index, int val) {
-        index+=n ; 
-        tree[index] = val ;
-        while(index>0){
-            
-            int left = index ;
-            int right = index ;
-            if(index%2==0)
-                right = index+1 ;
-            else
-                left = index - 1 ; 
-            
-            tree[index/2] = tree[left] + tree[right] ;
-            index/=2 ;
-        }
+        sum-=v[index];     //subtract old element  from sum at index and then update by adding new element val.
+        v[index]=val;
+        sum+=val;
         
     }
     
     int sumRange(int left, int right) {
-        left += n ;
-        
-        right += n ;
-        
-        int sum =0 ;
-        while(left<=right){
-            
-            if(left%2==1) //it is right child of someone
-            {
-                sum+=tree[left] ; 
-                left++ ;
-            }
-            if(right%2==0)
-            {
-                sum+=tree[right] ;
-                right-- ;
-            }
-            left/=2 ;
-            right/=2 ;
+        int res=sum; 
+        for(int i=0;i<left;i++){    //subtract all element before left and after right
+            res-=v[i];
         }
-        return sum ;
+        for(int i=right+1;i<v.size();i++){
+            res-=v[i];
+        }
+        return res;// return res ie. our answer.
     }
 };
-
-/**
- * Your NumArray object will be instantiated and called as such:
- * NumArray* obj = new NumArray(nums);
- * obj->update(index,val);
- * int param_2 = obj->sumRange(left,right);
- */
