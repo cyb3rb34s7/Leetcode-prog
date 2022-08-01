f4c7b9704a0889c0c625c479e178e47a3fd7d55d
Cyb3rb34s7 <56477046+cyb3rb34s7@users.noreply.github.com>
2022-08-01 13:54:53 +0530

    Time: 12 ms (79.60%), Space: 11.8 MB (98.18%) - LeetHub

--- 150-evaluate-reverse-polish-notation/150-evaluate-reverse-polish-notation.cpp
+++ 150-evaluate-reverse-polish-notation/150-evaluate-reverse-polish-notation.cpp
@@ -1,49 +1,26 @@
 class Solution {
 public:
-    int to_int(string &s){
-        int sum = 0 ;
-        bool sign = false;
-        if(s[0]=='-')
-            sign = true ;
-        for(int i=0;i<s.size();i++){
-            if(sign and i==0) continue;
-            sum = sum*10 + s[i] -'0' ; 
-        }
-        sum = (sign) ? sum*-1 : sum ;
-        return sum ;
-    }
-  
     int evalRPN(vector<string>& tokens) {
-     
-        stack<int> st ;
+     stack<int> st ; 
         
-        for(int i=0;i<tokens.size();i++){
-            if(tokens[i]=="+")
+        for(auto x:tokens){
+            
+            if(x=="*" or x=="-" or x=="+" or x=="/"){
+                
+                int a = st.top() ; st.pop() ;
+                int b = st.top()  ; st.pop() ;
+                
+                int ans ; 
+                if(x=="*") ans = b*a ;
+                else if(x=="-") ans = b-a ;
+                else if(x=="+") ans = b+a ;
+                else if(x=="/") ans = b/a ;
+                
+                st.push(ans) ;
+             }
+            else
             {
-                int a = st.top() ; st.pop() ;
-                int b = st.top() ; st.pop() ;
-                st.push(b+a) ;
-            }
-            else if(tokens[i]=="-"){
-                 
-            
-                int a = st.top() ; st.pop() ;
-                int b = st.top() ; st.pop() ;
-                st.push(b-a) ;
-            
-            }
-            else if(tokens[i]=="*"){
-                 int a = st.top() ; st.pop() ;
-                int b = st.top() ; st.pop() ;
-                st.push(b*a) ;
-            }
-            else if(tokens[i]=="/"){
-                 int a = st.top() ; st.pop() ;
-                int b = st.top() ; st.pop() ;
-                st.push(b/a) ;
-            }
-            else{
-                st.push(to_int(tokens[i])) ;
+                st.push((stoi)(x)) ; 
             }
             
         }
