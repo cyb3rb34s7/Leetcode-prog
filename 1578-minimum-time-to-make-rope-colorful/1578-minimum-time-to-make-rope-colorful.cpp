1c9faf05e6fbb2b0b66d6fed80911e08fdbacb8c
Cyb3rb34s7 <56477046+cyb3rb34s7@users.noreply.github.com>
2022-08-05 11:46:09 +0530

    Time: 282 ms (32.61%), Space: 95.5 MB (51.55%) - LeetHub

--- 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.cpp
+++ 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.cpp
@@ -1,18 +1,17 @@
 class Solution {
 public:
     int minCost(string s, vector<int>& arr) {
-        int lastMin = arr[0] ;
+       
         int ans = 0 ;
         
         for(int i=1;i<s.size();i++){
             
             if(s[i]==s[i-1])
             {
-                ans+= min(lastMin,arr[i]) ;
-                lastMin = max(lastMin,arr[i]) ;
+                ans+= min(arr[i-1],arr[i]) ;
+                arr[i] = max(arr[i-1],arr[i]) ;
             }
-            else
-                lastMin = arr[i] ;
+            
         }
         return ans; 
     }
