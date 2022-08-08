d96cbc0eba3e951a3a07002fe4ee0556662c8275
Cyb3rb34s7 <56477046+cyb3rb34s7@users.noreply.github.com>
2022-08-08 23:57:09 +0530

    Time: 8 ms (45.74%), Space: 12.1 MB (36.55%) - LeetHub

--- 31-next-permutation/31-next-permutation.cpp
+++ 31-next-permutation/31-next-permutation.cpp
@@ -1,6 +1,35 @@
-class Solution {
-public:
-    void nextPermutation(vector<int>& nums) {
-        next_permutation(nums.begin(),nums.end()) ;
-    }
+class Solution
+{
+    public:
+        void nextPermutation(vector<int> &nums)
+        {
+            int ind = -1;
+
+            for (int i = nums.size() - 1; i >= 1; i--)
+            {
+
+                if (nums[i - 1] < nums[i])
+                {
+                    ind = i - 1;
+                    break;
+                }
+            }
+            if (ind == -1)
+            {
+                reverse(nums.begin(), nums.end());
+                return;
+            }
+            int i;
+            for (i = nums.size() - 1; i >= 0; i--)
+            {
+                if (nums[i] > nums[ind])
+                {
+                    break;
+                }
+            }
+            swap(nums[ind], nums[i]);
+            reverse(nums.begin() + ind + 1, nums.end());
+
+            return;
+        }
 };
