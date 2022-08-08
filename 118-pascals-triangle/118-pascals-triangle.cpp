c4f569a0edf74958d9ec92c5ba3df0a8cf4532be
Cyb3rb34s7 <56477046+cyb3rb34s7@users.noreply.github.com>
2022-08-08 22:19:48 +0530

    Time: 3 ms (39.24%), Space: 6.2 MB (99.37%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.cpp
+++ 118-pascals-triangle/118-pascals-triangle.cpp
@@ -1,16 +1,22 @@
 class Solution {
 public:
-    vector<vector<int>> generate(int numRows) {
-        vector<vector<int>> r(numRows);
+    vector<vector<int>> generate(int n) {
+        vector<vector<int>> res ; 
         
-        for(int i=0;i<numRows;i++){
-            r[i].resize(i+1) ;
-            r[i][0]=r[i][i]=1 ; 
-            
-            for(int j=1;j<i;j++){
-                r[i][j]=r[i-1][j-1]+r[i-1][j];
+        int dp[n][n]; 
+        memset(dp,0,sizeof(dp)) ;
+        
+        dp[0][0] = 1 ;
+        res.push_back({1}) ;
+        for(int i=1;i<n;i++){
+            vector<int> temp(i+1) ;
+            temp[0] = 1 ;
+            temp[i] = 1 ;
+            for(int j =1 ;j < i ;j++){
+                temp[j] = res[i-1][j-1] + res[i-1][j] ;
             }
+            res.push_back(temp) ;
         }
-        return r;
+        return res ; 
     }
 };
