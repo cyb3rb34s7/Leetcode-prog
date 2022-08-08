f9fadb6fd6612485bfab2edc9671b453ee3e0109
Cyb3rb34s7 <56477046+cyb3rb34s7@users.noreply.github.com>
2022-08-08 22:00:02 +0530

    Time: 31 ms (22.26%), Space: 13.1 MB (81.84%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -3,27 +3,50 @@ public:
     void setZeroes(vector<vector<int>>& matrix) {
         int m = matrix.size() ;
         int n= matrix[0].size() ;
-        vector<int> col(n,1) ;
-        vector<int> row(m,1) ; 
-        
-        
+        bool isCol =false, isRow =false ;
         for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
+            if(matrix[i][0]==0)
+                {isCol = true ;
+                    break;
+                }
+        }
+        
+        for(int j=0;j<n;j++){
+            if(matrix[0][j]==0)
+            {
+                isRow = true; 
+                break ;
+            }
+        }
+        
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
                 if(matrix[i][j]==0)
                 {
-                    col[j] = 0 ;
-                    row[i] = 0 ;
+                    matrix[i][0]=0 ;
+                    matrix[0][j] = 0 ;
                 }
                 
             }
         }
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                
-                if(row[i]==0 or col[j]==0)
-                    matrix[i][j] = 0; 
+        
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                if(matrix[i][0]==0 or matrix[0][j]==0)
+                    matrix[i][j] = 0;
             }
-            
+        }
+        if(isCol)
+        {
+            for(int i=0;i<m;i++){
+                matrix[i][0] = 0;
+            }
+        }
+        if(isRow)
+        {
+            for(int j=0;j<n;j++){
+                matrix[0][j]=0 ;
+            }
         }
         
     }
