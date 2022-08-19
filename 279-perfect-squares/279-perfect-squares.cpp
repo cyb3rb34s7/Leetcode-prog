b84835083f1ccc2304e436a11333468a23135a35
Cyb3rb34s7 <56477046+cyb3rb34s7@users.noreply.github.com>
2022-08-19 15:41:05 +0530

    Time: 367 ms (36.39%), Space: 7.3 MB (82.36%) - LeetHub

--- 279-perfect-squares/279-perfect-squares.cpp
+++ 279-perfect-squares/279-perfect-squares.cpp
@@ -1,32 +1,28 @@
 class Solution {
 public:
-    
-    int solve(int ind,int w ,vector<int> &res,vector<vector<int>> &dp){
-        
-        if(w==0)
+    int dp[10005];
+    int go(int idx,vector<int>&res,int n){
+        int ans = INT_MAX;
+        if(idx==n)
             return 0 ;
-        if(ind<0 and w!=0){
-            return 1e6 ;
+        if(dp[idx]!=-1)
+            return dp[idx] ;
+        for(int i=0;i<res.size();i++){
+            if(res[i]+idx<=n){
+                ans = min(ans,go(idx+res[i],res,n)+1) ;
+            }
         }
-        if(dp[ind][w]!=-1)
-            return dp[ind][w] ;
-        int notTake = solve(ind-1,w,res,dp) ;
-        int take = 1e6 ;
-        if(res[ind]<=w){
-             take = 1 + solve(ind,w-res[ind],res,dp) ;
-        }
-        return dp[ind][w] = min(take,notTake) ;
+        return dp[idx]= ans ;
     }
     int numSquares(int n) {
-        vector<int> res ;
         
-        for(int i=1 ;i*i<=n ;i++)
-        {
+        vector<int>res ;
+        memset(dp,-1,sizeof(dp)) ;
+        for(int i=1;i*i <= n ;i++){
             res.push_back(i*i) ;
         }
         
-        int N = res.size() , W = n ;
-        vector<vector<int>> dp(N+1,vector<int>(W+1,-1)) ;
-        return solve(N-1,W,res,dp) ;
+        return go(0,res,n) ; 
+        
     }
 };
