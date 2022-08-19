f776d377550b66cd9c6302bc3636cbaec8095f14
Cyb3rb34s7 <56477046+cyb3rb34s7@users.noreply.github.com>
2022-08-19 11:56:42 +0530

    Time: 336 ms (53.69%), Space: 81 MB (30.94%) - LeetHub

--- 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.cpp
+++ 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.cpp
@@ -7,20 +7,21 @@ public:
             mp[x]++ ; 
         }
         
-        vector<pair<int,int>> vec ;
+        priority_queue<pair<int,int>> pq ;
         
         for(auto it:mp){
-            vec.push_back({it.second,it.first}) ;
+            pq.push({it.second,it.first}) ;
         }
-        sort(vec.begin(),vec.end()) ;
-        int cnt = 0 ; 
+       int cnt =0 ;
         int n = 0 ;
-        for(int i = vec.size()-1;i>=0;i--){
-           n+=vec[i].first ;
-            cnt++;
-            if(n>=arr.size()/2) 
-                return cnt ;
+        
+        while(n<arr.size()/2){
+            n+=pq.top().first ;
+            cnt++ ;
+            pq.pop();
+            
         }
+       
         return cnt ;
     }
 };
