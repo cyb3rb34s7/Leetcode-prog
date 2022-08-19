6496b9ee243da499bd4ee55711118fb460de588c
Cyb3rb34s7 <56477046+cyb3rb34s7@users.noreply.github.com>
2022-08-19 19:37:55 +0530

    Time: 86 ms (56.04%), Space: 22.4 MB (31.17%) - LeetHub

--- 2266-count-number-of-texts/2266-count-number-of-texts.cpp
+++ 2266-count-number-of-texts/2266-count-number-of-texts.cpp
@@ -3,15 +3,7 @@ public:
     int n;
     int dp[100005] ;
     int mod = 1e9+7 ; 
-    bool isSame(string x){
-        
-        for(int i=1;i<x.size();i++){
-            if(x[i]!=x[i-1])
-                return false;
-        }
-        return true ;
-            
-    }
+   
     int go(int idx,string &s){
         
         if(idx>=n)
@@ -20,9 +12,8 @@ public:
         if(dp[idx]!=-1)
             return dp[idx] ;
         int bound = (s[idx]-'0'==7 or s[idx]-'0'==9) ? 4 : 3 ;
-        for(int i=1;i<=bound;i++){
-            
-            if(idx+i<=n and isSame(s.substr(idx,i)) )
+        for(int i=1;i<=bound && idx+i<=n && s[idx]==s[idx+i-1];i++){
+
                 ans=(ans+(go(idx+i,s))%mod)%mod;
            
         }
