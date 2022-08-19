949edc036cc94c2ddc7372c352bad6306393bfa6
Cyb3rb34s7 <56477046+cyb3rb34s7@users.noreply.github.com>
2022-08-19 19:42:22 +0530

    Time: 110 ms (35.81%), Space: 26.1 MB (18.37%) - LeetHub

--- 2266-count-number-of-texts/2266-count-number-of-texts.cpp
+++ 2266-count-number-of-texts/2266-count-number-of-texts.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
     int n;
-    int dp[100005] ;
+    
     int mod = 1e9+7 ; 
    
-    int go(int idx,string &s){
+    int go(int idx,string &s,vector<int> &dp){
         
         if(idx>=n)
             return 1;
@@ -14,14 +14,15 @@ public:
         int bound = (s[idx]-'0'==7 or s[idx]-'0'==9) ? 4 : 3 ;
         for(int i=1;i<=bound && idx+i<=n && s[idx]==s[idx+i-1];i++){
 
-                ans=(ans+(go(idx+i,s))%mod)%mod;
+                ans=(ans+(go(idx+i,s,dp))%mod)%mod;
            
         }
         return dp[idx] = ans % mod ;
     }
     int countTexts(string s) {
         n = s.size() ;
-        memset(dp,-1,sizeof(dp)) ;
-        return go(0,s);
+        vector<int> dp(n+1,-1) ;
+
+        return go(0,s,dp);
     }
 };
