22dcd513642333cc349e7b84a6e999ff8461baad
Cyb3rb34s7 <56477046+cyb3rb34s7@users.noreply.github.com>
2022-08-19 16:40:45 +0530

    Time: 572 ms (12.91%), Space: 42.6 MB (36.74%) - LeetHub

--- 2222-number-of-ways-to-select-buildings/2222-number-of-ways-to-select-buildings.cpp
+++ 2222-number-of-ways-to-select-buildings/2222-number-of-ways-to-select-buildings.cpp
@@ -1,26 +1,28 @@
-class Solution {
-public:
-    int n;
+class Solution
+{
+    public:
+        int n;
     long long dp[100005][3][4];
-    long long go(int idx,int prevChar,int cnt,string &s){
-        if(cnt==3)
+    long long go(int idx, int prevChar, int cnt, string &s)
+    {
+        if (cnt == 3)
             return 1;
-        if(idx>=n)
-            return 0 ;
-        
-        if(dp[idx][prevChar][cnt]!=-1)
+        if (idx >= n)
+            return 0;
+
+        if (dp[idx][prevChar][cnt] != -1)
             return dp[idx][prevChar][cnt];
-        long long pick = 0;
-        if(cnt<=2 and s[idx]-'0'!=prevChar)
-             pick = go(idx+1,s[idx]-'0',cnt+1,s) ;
-        long long notPick = go(idx+1,prevChar,cnt,s);
-        
-        return dp[idx][prevChar][cnt] = pick+notPick ;
-        
+        long long ans = 0;
+        if (cnt <= 2 and s[idx] - '0' != prevChar)
+            ans += go(idx + 1, s[idx] - '0', cnt + 1, s);
+        ans += go(idx + 1, prevChar, cnt, s);
+
+        return dp[idx][prevChar][cnt] = ans;
     }
-    long long numberOfWays(string s) {
-        n= s.size() ;
-        memset(dp,-1,sizeof(dp)) ;
-        return go(0,2,0,s);
+    long long numberOfWays(string s)
+    {
+        n = s.size();
+        memset(dp, -1, sizeof(dp));
+        return go(0, 2, 0, s);
     }
 };
