023e77e20111043bca2615156a230163ba2be972
Cyb3rb34s7 <56477046+cyb3rb34s7@users.noreply.github.com>
2022-08-19 16:02:07 +0530

    Time: 3 ms (38.94%), Space: 5.9 MB (80.91%) - LeetHub

---
+++ 357-count-numbers-with-unique-digits/357-count-numbers-with-unique-digits.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int countNumbersWithUniqueDigits(int n) {
+        if(n==0)
+            return 1 ;
+        
+        int ans = 10  ; //base case for 1 digit ;
+        int curr=9;
+        int start = 9 ;
+        
+        while(n-->1 and start!=0){
+            
+            curr*=(start--) ; 
+            ans+=curr ;
+        }
+        return ans ;
+        
+    }
+};
