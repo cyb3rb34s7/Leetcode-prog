e00c2e2ef37276ecaf05f73831f3705bffed6339
Cyb3rb34s7 <56477046+cyb3rb34s7@users.noreply.github.com>
2022-08-20 08:58:53 +0530

    Time: 67 ms (90.49%), Space: 9.3 MB (59.64%) - LeetHub

--- 823-binary-trees-with-factors/823-binary-trees-with-factors.cpp
+++ 823-binary-trees-with-factors/823-binary-trees-with-factors.cpp
@@ -1,30 +1,34 @@
 class Solution {
 public:
+    #define ll long long
+    #define mod 1000000007
     int numFactoredBinaryTrees(vector<int>& arr) {
-        unordered_map<int,long long int> mp ;
+        unordered_map<int,ll> mp ;
         
-        long long mod =1e9+7 ;
+        for(auto x:arr)
+            mp[x] =1 ;
+        
         sort(arr.begin(),arr.end()) ;
-        for(int i=0;i<arr.size();i++){
-            mp[arr[i]] =1 ;
-        }
         
-        for(int i =1;i<arr.size();i++){
-            long long int cnt = 0 ;
-            for(int j=0 ;j<i;j++){
-                if(arr[i] % arr[j] == 0){
-                    if(mp.find(arr[i]/arr[j])!=mp.end())
-                        cnt+= (mp[arr[j]] * mp[arr[i]/arr[j]]) % mod ;
+        for(int i=1;i<arr.size();i++){
+            
+            ll cnt = 0 ;
+            
+            for(int j=0;j<i;j++){
+                
+                if(arr[i]%arr[j]!=0) continue;
+                
+                if(mp.find(arr[i]/arr[j])!=mp.end()){
                     
+                    cnt = (cnt + (mp[arr[j]] * mp[arr[i]/arr[j]]) %mod)%mod ; 
                 }
             }
-            mp[arr[i]]+=cnt % mod  ;
+            mp[arr[i]]+=(cnt %mod);
         }
-        long long int ans = 0;
+        ll ans = 0 ;
         for(auto it:mp){
             ans=(ans + it.second)%mod ;
         }
-        
-        return ans%mod ;
+        return ans ;
     }
 };
