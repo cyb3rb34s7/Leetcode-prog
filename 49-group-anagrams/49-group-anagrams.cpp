e34b984c81235cc6483cc0700be3cda98b6192d9
Cyb3rb34s7 <56477046+cyb3rb34s7@users.noreply.github.com>
2022-08-27 14:39:21 +0530

    Time: 63 ms (47.76%), Space: 20.7 MB (56.09%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.cpp
+++ 49-group-anagrams/49-group-anagrams.cpp
@@ -1,20 +1,23 @@
 class Solution {
 public:
-    vector<vector<string>> groupAnagrams(vector<string>& string_list) {
-         unordered_map<string,vector<string>> mp;
-        string temp;
-        for(auto x:string_list)
-        {
-            temp=x;
-            sort(x.begin(),x.end());
-            mp[x].push_back(temp);
-        }
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
         
-        vector<vector<string>> res;
         
-        for(auto itr=mp.begin();itr!=mp.end();itr++){
-            res.push_back(itr->second);
+        unordered_map<string,vector<string>> mp ;
+        
+        for(auto x:strs){
+            
+            string temp = x ;
+            sort(x.begin(),x.end()) ;
+            
+            mp[x].push_back(temp) ;
+            
         }
-        return res;
+        vector<vector<string>> res ;
+        
+        for(auto x:mp){
+            res.push_back(x.second) ;
+        }
+        return res ;
     }
 };
