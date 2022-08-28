4a77663fd948c12190cab76966131b27f20184c5
Cyb3rb34s7 <56477046+cyb3rb34s7@users.noreply.github.com>
2022-08-28 19:46:33 +0530

    Time: 10 ms (31.12%), Space: 12.2 MB (36.63%) - LeetHub

--- 31-next-permutation/31-next-permutation.cpp
+++ 31-next-permutation/31-next-permutation.cpp
@@ -1,35 +1,31 @@
-class Solution
-{
-    public:
-        void nextPermutation(vector<int> &nums)
-        {
-            int ind = -1;
-
-            for (int i = nums.size() - 1; i >= 1; i--)
-            {
-
-                if (nums[i - 1] < nums[i])
-                {
-                    ind = i - 1;
-                    break;
-                }
-            }
-            if (ind == -1)
-            {
-                reverse(nums.begin(), nums.end());
-                return;
-            }
-            int i;
-            for (i = nums.size() - 1; i >= 0; i--)
-            {
-                if (nums[i] > nums[ind])
-                {
-                    break;
-                }
-            }
-            swap(nums[ind], nums[i]);
-            reverse(nums.begin() + ind + 1, nums.end());
-
-            return;
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        
+        int n =nums.size() ;
+        
+        int i = n-1;
+        
+        while(i>0 and nums[i]<=nums[i-1]){
+            i-- ;
         }
+        
+        int last = i-1 ;
+        if(last <= -1)
+        {
+            reverse(nums.begin(),nums.end()) ;
+            return ;
+        }
+        int maxx ;
+        for(int i=last+1;i<n;i++){
+            if(nums[i]>nums[last])
+                maxx = i;
+        }
+
+        swap(nums[last],nums[maxx]) ;
+       
+        reverse(nums.begin()+last+1,nums.end()) ;
+        
+        
+    }
 };
