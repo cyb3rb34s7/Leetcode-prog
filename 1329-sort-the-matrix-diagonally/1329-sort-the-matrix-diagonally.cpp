cc0329cda20b3e202d5a7a441e0f577996a098d9
Cyb3rb34s7 <56477046+cyb3rb34s7@users.noreply.github.com>
2022-08-28 11:02:25 +0530

    Time: 31 ms (12.63%), Space: 9.5 MB (28.08%) - LeetHub

--- 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.cpp
+++ 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.cpp
@@ -1,38 +1,23 @@
 class Solution {
 public:
     vector<vector<int>> diagonalSort(vector<vector<int>>& mat) {
-        int m = mat.size() ;
-        int n = mat[0].size() ;
+        int m=mat.size() , n = mat[0].size() ; 
+        
+        unordered_map<int,priority_queue<int,vector<int>,greater<int>>> mp ;
         
         for(int i=0;i<m;i++){
-            
-            priority_queue<int,vector<int>,greater<int>> pq ;
-            for(int j=0,x=i;j<n && x<m ;x++,j++){
-                pq.push(mat[x][j]) ;
+            for(int j=0;j<n;j++){
+                
+                mp[i-j].push(mat[i][j]) ;
             }
-            
-            for(int j=0,x=i;j<n && x<m ;x++,j++){
-                mat[x][j] = pq.top() ;
-                pq.pop() ;
-            }
-            
         }
-        
-        for(int j=1;j<n;j++){
-            
-            priority_queue<int,vector<int>,greater<int>> pq ;
-            for(int y = j,i=0;y<n && i<m ;y++,i++){
-                pq.push(mat[i][y]) ;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                
+               mat[i][j] = mp[i-j].top() ;
+                mp[i-j].pop() ;
             }
-            
-            for(int y = j,i=0;y<n && i<m ;y++,i++){
-                mat[i][y] = pq.top() ;
-                pq.pop() ;
-            }
-            
         }
-       
-    return mat;
+        return mat; 
     }
-    
 };
