cf36f048570af886e36fd4d65585c49416026df8
Cyb3rb34s7 <56477046+cyb3rb34s7@users.noreply.github.com>
2022-08-28 20:23:25 +0530

    Time: 79 ms (26.16%), Space: 19 MB (85.30%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.cpp
+++ 56-merge-intervals/56-merge-intervals.cpp
@@ -1,25 +1,27 @@
 class Solution {
 public:
-    vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        sort(intervals.begin(),intervals.end());
-
-        int low= intervals[0][0] ,high=intervals[0][1] ; 
-        vector<vector<int>> res;
-        for(int i=1;i<intervals.size();i++){
-            if(intervals[i][0]<=high){
-                low= min(low,intervals[i][0]);
-                high=max(high,intervals[i][1]);
-            }
-                
-            else
-            {
-                res.push_back({low,high});
-                low= intervals[i][0] ;
-                high= intervals[i][1] ;
-            }
-        }
-        res.push_back({low,high});
+    vector<vector<int>> merge(vector<vector<int>>& arr) {
         
-        return res;
+        int m =arr.size() ;
+        sort(arr.begin(),arr.end()) ;
+        
+        vector<vector<int>> res ;
+        
+        int low = arr[0][0], high = arr[0][1] ;
+        
+        for(int i=1;i<m;i++){
+            if(arr[i][0]<=high)
+            {
+                high = max(high,arr[i][1]) ;
+
+            }
+            else{
+                    res.push_back({low,high}) ;
+                    low = arr[i][0] ;
+                    high = arr[i][1] ;
+                }
+        }
+        res.push_back({low,high}) ;
+        return res; 
     }
 };
